63b2512ad46cbce67f3b40f1fbe7ddf65c11e14d
John <john.tan-aristy@csedge.org>
2025-02-18 10:13:32 -0600

    fix

--- objects/checkpoints/checkpoint.gd
+++ objects/checkpoints/checkpoint.gd
@@ -1,4 +1,4 @@
 extends Area2D
 @export var spawn_height = 400;
 func _on_body_exited(body: Node2D) -> void:
-	Global.p1_respawn_point = Vector2(body.global_position.x, spawn_height)
+	Global.p1_respawn_point = Vector2(self.global_position.x, spawn_height)
